45b4f064c68afe6f66fe43d0829e37898ba19765
Renzo Damian <renzo.damian.go@gmail.com>
2024-05-28 17:34:37 -0500

    Add image max width

--- src/components/products/product-card.tsx/ProductCard.tsx
+++ src/components/products/product-card.tsx/ProductCard.tsx
@@ -1,7 +1,8 @@
 import { Button } from "@/components/ui/button";
 import { ProductType } from "@/data/products.data";
-import { ShoppingBagIcon, ShoppingCart } from "lucide-react";
+import { ShoppingBagIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 type Props = {
@@ -10,29 +11,31 @@ type Props = {
 
 export const ProductCard = ({ product }: Props) => {
   return (
-    <div className="product-card rounded-2xl border border-white overflow-hidden flex flex-col h-[440px]">
-      <div className="flex-grow overflow-hidden h-[350px] bg-black">
-        <Image
-          className="object-cover w-[55%] md:w-[85%] mx-auto h-full object-center md:object-top   rounded-2xl shadow-inner"
-          src={product.image}
-          alt={product.name}
-          width={400}
-          height={300} // Adjusted for a fixed height aspect
-        />
-      </div>
-      <div className="px-6 pb-6 pt-6 md:px-4 md:py-4 flex flex-col justify-between flex-grow">
-        <div className="flex justify-between items-center font-semibold pb-2">
-          <h2>{product.name}</h2>
-          <p>S/{product.price.toFixed(2)}</p>
+    <Link href={`/products/${product.id}`} passHref>
+      <div className="product-card rounded-2xl border border-white overflow-hidden flex flex-col h-[440px] transition duration-300 ease-in-out transform hover:scale-[102%]">
+        <div className="flex-grow overflow-hidden h-[350px] bg-black">
+          <Image
+            className="object-cover w-[55%] md:w-[85%] mx-auto h-full object-center md:object-top rounded-2xl shadow-inner max-w-[190px]"
+            src={product.image}
+            alt={product.name}
+            width={400}
+            height={300}
+          />
         </div>
-        <p className=" text-muted-foreground line-clamp-2 overflow-y-hidden mb-4">
-          {product.description}
-        </p>
-        <Button className="w-full mt-auto  ">
-          <ShoppingBagIcon className="mr-2 " height={20} width={20} /> Agregar
-          al carrito
-        </Button>
+        <div className="px-6 pb-6 pt-6 md:px-4 md:py-4 flex flex-col justify-between flex-grow">
+          <div className="flex justify-between items-center font-semibold pb-2">
+            <h2>{product.name}</h2>
+            <p>S/{product.price.toFixed(2)}</p>
+          </div>
+          <p className="text-muted-foreground line-clamp-2 overflow-hidden mb-4">
+            {product.description}
+          </p>
+          <Button className="w-full mt-auto">
+            <ShoppingBagIcon className="mr-2" height={20} width={20} />
+            Agregar al carrito
+          </Button>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
