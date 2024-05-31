96911898e5b1fe0cdf817427b5e4968267383ecd
Renzo Damian <renzo.damian.go@gmail.com>
2024-05-31 15:32:54 -0500

    edit footer position on desktop view

--- src/components/ui/footer/footer.tsx
+++ src/components/ui/footer/footer.tsx
@@ -1,3 +1,7 @@
 export const Footer = () => {
-  return <footer className=" bg-black px-6 text-center py-4">Black Comet @2024</footer>;
+  return (
+    <footer className=" bg-black px-6 text-center py-4 md:absolute md:bottom-0 w-full md:h-14">
+      Black Comet @2024
+    </footer>
+  );
 };
