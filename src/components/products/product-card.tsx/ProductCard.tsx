7c8885171db691af60a9e421b47d86d48a530432
Renzo Damian <renzo.damian.go@gmail.com>
2024-06-05 18:52:27 -0500

    change size of card

--- src/components/products/product-card.tsx/ProductCard.tsx
+++ src/components/products/product-card.tsx/ProductCard.tsx
@@ -13,7 +13,7 @@ type Props = {
 export const ProductCard = ({ product }: Props) => {
   return (
     <div className="product-card rounded-2xl border border-white overflow-hidden flex flex-col h-[440px] transition duration-300 ease-in-out transform hover:scale-[102%]">
-      <div className="flex-grow overflow-hidden h-[350px] bg-black">
+      <div className="flex-grow overflow-hidden h-[400px] bg-black">
         <Link href={`/product/${product.slug}`}>
           <Image
             className="  mx-auto h-full object-cover  object-center rounded-2xl shadow-inner "
