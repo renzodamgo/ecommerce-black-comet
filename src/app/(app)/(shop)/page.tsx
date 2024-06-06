a6298f6d965364c6bad9ff6ac35838d2aebb792f
Renzo Damian <renzo.damian.go@gmail.com>
2024-06-05 20:11:13 -0500

    hide console log

--- src/app/(app)/(shop)/page.tsx
+++ src/app/(app)/(shop)/page.tsx
@@ -4,7 +4,7 @@ import { MainHero } from "@/components/ui/hero/MainHero";
 
 export default async function Home() {
   const { docs } = await fetchProducts();
-  console.log(docs);
+  // console.log(docs);
   return (
     <main>
       <div className="mx-auto px-6 sm:px-14 max-w-[1200px] pt-4 grid gap-8 ">
